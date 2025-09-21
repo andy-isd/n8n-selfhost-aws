693cf6a1a4ac42f1c874cf88bd83ad6afa4b2897
Andrew Sergeyev <46224512+andy-isd@users.noreply.github.com>
2025-09-21 10:57:16 +0300

    remove comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # n8n-selfhost-aws
-This script is part of the manual of how to self-host n8n on AWS: https://andy.isd-group.com/n8n-free/
+This script is part of the manual of how to self-host n8n on AWS Free Tier: https://andy.isd-group.com/n8n-free/
--- n8n.sh
+++ n8n.sh
@@ -1,10 +1,8 @@
 # IMPORTANT: remember to replace YOURDOMAIN within this file with your registered subdomain
 
-# installing basic apps
 sudo apt update
 sudo install -y openssh-server dkms build-essential mc less htop zip unzip host wget curl net-tools rsync sqlite3
 
-# nginx
 sudo apt-get install -y nginx apache2-utils libnginx-mod-http-headers-more-filter
 sudo sed -i 's/# server_tokens off;/server_tokens off;\n\tmore_clear_headers Server;/g' /etc/nginx/nginx.conf
 sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/g' /etc/nginx/nginx.conf
@@ -13,33 +11,26 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /var/www/html
 sudo service nginx restart
 
-echo "Nodejs & NPM setup"
 curl -sL https://deb.nodesource.com/setup_20.x | sudo bash -
 sudo apt-get install -y nodejs
 sudo npm i n -g
 
-echo "Installing letsencrypt certbot"
 sudo apt-get install -y python3-acme python3-certbot python3-certbot-nginx python3-mock python3-openssl python3-pkg-resources python3-pyparsing python3-zope.interface
 
-# downloading files
-echo "Downloading files..."
 sudo wget -P /etc/nginx/sites-available https://raw.githubusercontent.com/andy-isd/n8n-selfhost-aws/refs/heads/main/etc/nginx/default
 sudo wget -P /var/www/default https://raw.githubusercontent.com/andy-isd/n8n-selfhost-aws/refs/heads/main/var/www/docker-compose.yml
 
 sudo chown root:www-data /var/www/default
 
-echo "Installing docker"
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo chmod +x get-docker.sh
 sudo ./get-docker.sh
 
-echo "Running n8n from docker container"
 sudo docker volume create n8n_data
 cd /var/www/default
 sudo docker compose up -d
 
 # IMPORTANT: replace YOURDOMAIN with your subdomain here
-echo "Setting up nginx host"
 sudo sed -i 's/DOMAIN/YOURDOMAIN/g' /etc/nginx/sites-available/default
 sudo ln -fns /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo service nginx reload
